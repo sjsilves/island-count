b3c1e97a3d7e3eb8af2c51ded42f6acbd9379199
Steven Silves <sjsilves@Stevens-MacBook-Pro-2.local>
2019-05-30 21:44:29 -0700

    rename and update readme

--- target/debug/deps/crossbeam_deque-dc3c480870d55fed.d
+++ target/debug/deps/crossbeam_deque-dc3c480870d55fed.d
@@ -1,5 +1,5 @@
-/Users/sjsilves/Documents/variables/target/debug/deps/libcrossbeam_deque-dc3c480870d55fed.rlib: /Users/sjsilves/.cargo/registry/src/github.com-1ecc6299db9ec823/crossbeam-deque-0.2.0/src/lib.rs
+/Users/sjsilves/Documents/git/island-count-iterate-collect/target/debug/deps/libcrossbeam_deque-dc3c480870d55fed.rlib: /Users/sjsilves/.cargo/registry/src/github.com-1ecc6299db9ec823/crossbeam-deque-0.2.0/src/lib.rs
 
-/Users/sjsilves/Documents/variables/target/debug/deps/crossbeam_deque-dc3c480870d55fed.d: /Users/sjsilves/.cargo/registry/src/github.com-1ecc6299db9ec823/crossbeam-deque-0.2.0/src/lib.rs
+/Users/sjsilves/Documents/git/island-count-iterate-collect/target/debug/deps/crossbeam_deque-dc3c480870d55fed.d: /Users/sjsilves/.cargo/registry/src/github.com-1ecc6299db9ec823/crossbeam-deque-0.2.0/src/lib.rs
 
 /Users/sjsilves/.cargo/registry/src/github.com-1ecc6299db9ec823/crossbeam-deque-0.2.0/src/lib.rs:
